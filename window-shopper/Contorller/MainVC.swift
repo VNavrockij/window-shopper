af8c95c8a199a61beb2eca0a9e23912cd18b8162
VNavrockij <vitalik.nav@gmail.com>
2022-05-02 12:34:18 +0300

    add: first Unit Test

--- window-shopper/Contorller/MainVC.swift
+++ window-shopper/Contorller/MainVC.swift
@@ -19,7 +19,7 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         
         let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
-        calcButton.backgroundColor = #colorLiteral(red: 0.8479229808, green: 0.2786461711, blue: 0.1192480549, alpha: 1)
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.3321625888, blue: 0.139706552, alpha: 0.9230265715)
         calcButton.setTitle("Calculate", for: .normal)
         calcButton.setTitleColor(#colorLiteral(red: 255.0, green: 255.0, blue: 255.0, alpha: 1), for: .normal)
         calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
